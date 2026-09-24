c4f7b89f41fa6c346f09acf719f7706f79bc4ce1
Himanshu Gupta <mehimanshu31@gmail.com>
2026-09-24 23:16:08 +0530

    chore(repo): clean temporary MCP exports, untrack redundant agent dirs & update gitignore/eslint

--- eslint.config.js
+++ eslint.config.js
@@ -6,10 +6,10 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', '.agents/**']),
 
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['src/**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
